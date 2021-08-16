9717db4b07889150f87e35c6aaa9cf3946780497
Carl Avidano <cavidano@gmail.com>
2021-08-16 11:58:07 -0400

    added react router

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React, {Fragment} from 'react'
+
+export const About = () => {
+    return (
+        <Fragment>
+            <h1>About This App</h1>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis ad ab dignissimos!
+            </p>
+        </Fragment>
+    )
+}

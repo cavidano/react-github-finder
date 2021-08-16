d7105dc77dcf8b38a50f81586fa49caed5c1da09
Carl Avidano <cavidano@gmail.com>
2021-08-16 06:20:09 -0400

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 
+// Components
+
+import Navbar from './components/layout/Navbar';
+
 import './App.css';
 
 class App extends Component {
 
   render() {
-
     return (
       <div className="App">
-        <h1>Hello From React</h1>
+        <Navbar title="Github Finder" icon="fab fa-github" />
+        <h1>Hello</h1>
       </div>
     );
   }

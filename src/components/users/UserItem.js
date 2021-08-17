b27603ee689c79c0c5657c6f8703f70ece2e409f
Carl Avidano <cavidano@gmail.com>
2021-08-17 04:34:34 -0400

    before adding user ui

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-const UserItem = ({user: {login, avatar_url, html_url}}) => {
+const UserItem = ({user: {login, avatar_url}}) => {
 
     return (
         <div className="card text-center">
             <img src={avatar_url} alt="" className="round-img" style={{ width: '80px' }} />
             <h3>{login}</h3>
             <div>
-                <a href={html_url} className="btn btn-dark btn-sm my-1">
+                <Link className="btn btn-dark btn-sm my-1" to={`/user/${login}`}>
                     More
-                </a>
+                </Link>
             </div>
         </div>
     )

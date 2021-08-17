ec148c77bceb3c1b6ba8b2eff3d3f56a2121bfcf
Carl Avidano <cavidano@gmail.com>
2021-08-17 05:02:07 -0400

    adding profile layout

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,8 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-export class User extends Component {
+class User extends Component {
+
     componentDidMount() {
         this.props.getUser(this.props.match.params.login);
+    }
+
+    static propTypes ={
+        loading: PropTypes.bool.isRequired,
+        user: PropTypes.object.isRequired,
+        getUser: PropTypes.func.isRequired
     }
 
     render() {
@@ -10,6 +20,7 @@ export class User extends Component {
         const {
             name,
             company,
+            location,
             avatar_url, 
             html_url, 
             bio, 
@@ -24,12 +35,115 @@ export class User extends Component {
 
         const { loading } = this.props;
 
+        if(loading) {
+            return <Spinner />
+        }
+
         return (
-            <div>
-                {name}
-            </div>
+            <Fragment>
+                <Link to="/" className="btn btn-light">
+                    Back To Search
+                </Link>
+
+                Hireable: {' '}
+                
+                { hireable ? (
+                    <i className="fas fa-check text-success" /> 
+                ) : (
+                    <i className="fas fa-times text-danger" />
+                )}
+
+                <div className="card grid-2">
+                    <div className="all-center">
+                        <img
+                            className="round-img"
+                            src={avatar_url}
+                            style={{ width: '150px'}}
+                            alt={name}
+                        />
+                        <h1>{name}</h1>
+                        <p>
+                            Location: {location}
+                        </p>
+                    </div>
+                    <div>
+                        {bio && (
+                        <Fragment>
+                            <h3>Bio</h3>
+                            <p>{bio}</p>
+                        </Fragment>
+                        )}
+                        <a href={html_url} className="btn btn-dark my-1">
+                            View GitHub Profile
+                        </a>
+                        <ul>
+                            <li>
+                                {login && (
+                                    <Fragment>
+                                        <strong>Username: </strong> {login}
+                                    </Fragment>
+                                )}
+                            </li>
+                            <li>
+                                {login && (
+                                    <Fragment>
+                                        <strong>Company: </strong> {company}
+                                    </Fragment>
+                                )}
+                            </li>
+                            <li>
+                                {login && (
+                                    <Fragment>
+                                        <strong>Website: </strong> {company}
+                                    </Fragment>
+                                )}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="card text-center">
+                    <div className="badge badge-primary">
+                        Followers: {followers}
+                    </div>
+                    <div className="badge badge-success">
+                        Following: {following}
+                    </div>
+                    <div className="badge badge-light">
+                        Public Repos: {public_repos}
+                    </div>
+                    <div className="badge badge-dark">
+                        Public Gists: {public_gists}
+                    </div>
+                </div>
+            </Fragment>
         )
     }
 }
 
 export default User;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
